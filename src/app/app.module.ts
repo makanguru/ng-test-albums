dc83c47572e4a67e468d582f59b857b45a4b3c27
unknown <makprg@ukr.net>
2020-06-22 10:49:46 +0300

    create user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserItemComponent } from './users/user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
