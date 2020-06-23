b927893da68eae3c3e9850f28cef3308c9aa1e5b
unknown <makprg@ukr.net>
2020-06-23 09:39:17 +0300

    appear change

--- src/app/albums/album-item/album-item.component.ts
+++ src/app/albums/album-item/album-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { AlbumService } from '../../services/album.service';
+import { Album } from '../../models/album';
 
 @Component({
   selector: 'app-album-item',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumItemComponent implements OnInit {
 
-  constructor() { }
+  album: Album;
+
+  constructor(
+    private route: ActivatedRoute,
+    private albumService: AlbumService
+  ) { }
 
   ngOnInit(): void {
+    this.getAlbum();
   }
+
+  getAlbum(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.albumService.getAlbum(id)
+      .subscribe( album => {
+        this.album = album['result'];
+      });
+  }
+
+
 
 }
